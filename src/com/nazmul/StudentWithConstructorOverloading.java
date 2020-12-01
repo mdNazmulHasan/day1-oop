c8589c9f6d3e7eeb95876fc79d2a1e9ef1727898
mdNazmulHasan <najmul.hasan10@gmail.com>
2020-12-01 15:52:19 +0600

    created class with  constructor overloading

---
+++ src/com/nazmul/StudentWithConstructorOverloading.java
@@ -0,0 +1,14 @@
+package com.nazmul;
+
+public class StudentWithConstructorOverloading {
+    String firstName;
+    String lastName;
+
+    public StudentWithConstructorOverloading(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public StudentWithConstructorOverloading() {
+    }
+}
