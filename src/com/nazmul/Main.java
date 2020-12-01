e9964c3aeb6fb9795db34c372b9efa68f9e0190a
mdNazmulHasan <najmul.hasan10@gmail.com>
2020-12-01 11:51:34 +0600

    Initial commit

---
+++ src/com/nazmul/Main.java
@@ -0,0 +1,12 @@
+package com.nazmul;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Student firstStudent = new Student();
+        firstStudent.firstName = "Android";
+        firstStudent.lastName = "Studio";
+        System.out.println(firstStudent.firstName + firstStudent.lastName);
+    }
+}
---
+++ src/com/nazmul/Student.java
@@ -0,0 +1,6 @@
+package com.nazmul;
+//Learn how to create Class and its Object
+public class Student {
+    String firstName;
+    String lastName;
+}
